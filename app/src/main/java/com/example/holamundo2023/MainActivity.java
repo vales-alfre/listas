229a141c2da2aff278be3ad8bf5f7dfb444bd92d
vales-alfre <vchicav@uteq.edu.ec>
2023-06-14 19:47:18 -0500

    mejor lo que esa

--- app/src/main/java/com/example/holamundo2023/MainActivity.java
+++ app/src/main/java/com/example/holamundo2023/MainActivity.java
@@ -7,7 +7,11 @@ import android.os.Bundle;
 import android.text.Editable;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.Toast;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,9 +24,31 @@ public class MainActivity extends AppCompatActivity {
     public void btEnviar (View view)
     {
         EditText txtNombre=(EditText) findViewById(R.id.txtNombre);
-        Toast.makeText(this.getApplicationContext(),"hola "+txtNombre.getText().toString(),Toast.LENGTH_LONG).show();
+        EditText TxtContra=(EditText) findViewById(R.id.TxtContra);
+        RadioButton rbMac =(RadioButton) findViewById(R.id.rbMasc);
+        Switch Noti=(Switch) findViewById(R.id.swNotificaciones);
+
+        String Nombre = txtNombre.getText().toString();
+        String Contraseña= TxtContra.getText().toString();
+        String Genero =rbMac.isChecked()?"Masculino":"Femenino";
+        String Notifi = Noti.isChecked()?"Si": "No";
+        Toast.makeText(this.getApplicationContext(),
+                "Nombre "+Nombre+"\n"+
+                        "Contraseña"+Contraseña+"\n"+
+                        "Genero"+Genero+"\n"+
+                        "Notificaciones"+Notifi,Toast.LENGTH_LONG).show();
+        Intent intent = new Intent(MainActivity.this, MainActivity2.class);
+        Bundle b = new Bundle();
+        b.putString("Nombre",Nombre);
+        b.putString("Contraseña",Contraseña);
+        b.putString("Genero",Genero);
+        b.putString("Notificaciones",Notifi);
+        intent.putExtras(b);
+        startActivity(intent);
+
 
     }
+
 }
 
 
---
+++ app/src/main/java/com/example/holamundo2023/MainActivity2.java
@@ -0,0 +1,15 @@
+package com.example.holamundo2023;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+    }
+
+}
